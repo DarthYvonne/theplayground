e93d2d08c588ede24799aba27817e98345222272
DarthYvonne <anderscolding@gmail.com>
2026-05-25 21:09:16 +0200

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>{{ $title ?? 'The Playground' }}</title>
-<link rel="icon" type="image/png" href="{{ asset('img/playground_logo.png') }}">
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
 <style>
